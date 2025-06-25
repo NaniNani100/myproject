23e2e7990a9e70f199854edff6ab63f1feddd17b
NaniNani100 <nanin18600@gmail.com>
2025-06-25 15:22:06 +0530

    dom

---
+++ dom.js
@@ -0,0 +1,11 @@
+document.title="Dom in javascript";
+document.getElementById("header").innerHTML="hello world";
+document.getElementById("para").style.color="blue";
+document.getElementById("para").style.fontSize="30px";
+document.getElementById("mr").style.color="red";
+document.body.style.backgroundColor="yellow";
+document.getElementById("csd").style.color="green";
+document.getElementsByTagName("h2")[2].style.color="red";
+document.getElementsByClassName("cls")[0].style.color="red";
+document.getElementsByClassName("cls").style.fontSize="50px";
+
