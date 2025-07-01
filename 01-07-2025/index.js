44ce4813e3739d3e4ca61a75d338f1bc2d9d96f7
NaniNani100 <nanin18600@gmail.com>
2025-07-01 15:34:51 +0530

    promise-catch

---
+++ 01-07-2025/index.js
@@ -0,0 +1,124 @@
+// console.log("hello world");
+// function sayHello(){
+//     console.log("hello from sayhello function");
+// }
+// sayHello();
+// console.log("this is a simple js progranm");
+
+const { getTemplateInstallPackage } = require("create-react-app/createReactApp");
+
+// console.log("one");
+// console.log("two");
+// setTimeout(()=>{
+//     console.log("hello world");
+// },5000);
+// console.log("three");
+// console.log("four");
+
+// function sum(a,b){
+//     console.log(a+b)
+// }
+// function calculator(a,b,ds){
+//   ds(a,b);
+// }
+// calculator(5,10,sum);
+
+// sum(5,10(a,b)=>{
+//console.log(a+b)    
+//});
+
+//nested loops 
+// for (let i=0;i<5;i++){
+//     let str="";
+//     for(let j=0;j<5;j++){
+//         str+=j;
+//     }
+//     console.log(1,str);
+// } 
+// function getdata(dataid){
+//     setTimeout(()=>{
+//         console.log("fetching data for id:",dataid);
+//     },5000);
+// }
+// getdata(1);
+// getdata(2);
+// getdata(3);
+
+function getdata(dataid,getnextdata){
+    setTimeout(()=>{
+    console.log("fetching data for id:",dataid);
+      if(getnextdata){
+      getnextdata();
+      } 
+    },2000);
+}
+
+getdata(1,()=>{
+    console.log("getting data2....")
+    getdata(2,()=>{
+        console.log("getting data3....")
+        getdata(3);
+    });
+});
+
+// callbackhell nested  call back
+//to over come this we wil use promises
+// promise is eventually completion of an operation
+// promise is an object that represnts the eventual completion 
+// or failure of an asynchronous operation and its resulting value.
+// it allows you to write asynchronous code in amore manageable way, avoiding callback hell.
+
+
+
+//promise:syntax
+//const mypromise = new Promise((resolve,reject )=>.{..})
+//resolve is called when the asynchronous operation is successful 
+//reject is called when the asynchronous operation fails.
+
+// const mypromise=new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         const success = false;
+//         if(success){ 
+//             resolve("data fetched successfully");
+
+//         }else{
+//             reject("error fetching data");
+//         }
+//     },2000);
+// });
+// mypromise.then((data)=>{
+//     console.log("promise resloved with data:",data);
+// })
+// .catch((error)=>{
+//     console.error("promise rejected with errors:",error);
+// })
+
+let promise=new Promise((resolve,reject)=>{
+    console.log("waste fellow");
+    if(1<0){
+        resolve(123);
+    }
+    reject("something went wrong");
+});
+
+
+function getdata(dataid){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log("fetching data fo id ",dataid);
+            resolve("sucess");
+        },2000);
+    });
+}
+let result =getdata(123);
+result;
+
+getdata(101)
+.then(result=>{
+    console.log("first fetch",result);
+    return getdata(102);
+})
+.then(result => {
+    console.log("second fetch",result);
+    return getdata(103);
+});
